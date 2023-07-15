48aa3c272fc2a92ee1c60451bf7628ae28e39b69
ibraham-vicdigit <96982180+ibrahim1165@users.noreply.github.com>
2023-07-15 16:50:15 +0530

    header added

--- src/components/sherd/Layout.js
+++ src/components/sherd/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Sidber from './Sidber'
+import Header from './Header'
 
 const Layout = () => {
     return (
@@ -9,14 +10,12 @@ const Layout = () => {
                 <Sidber />
 
 
-            <div className="p-4">
-                <div className='bg-teal-200'>
-                    header
-                </div> 
-            <div>
-                <Outlet />
-            </div>
-            </div>
+                <div className="flex flex-col flex-1">
+				<Header />
+				<div className="flex-1 p-4 min-h-0 overflow-auto">
+					<Outlet />
+				</div>
+			</div>
 
 
 
