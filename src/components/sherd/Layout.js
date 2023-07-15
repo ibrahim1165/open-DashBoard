ee8ac5390f9dace145047a00152be113058af207
ibraham-vicdigit <96982180+ibrahim1165@users.noreply.github.com>
2023-07-15 18:33:57 +0530

    fixed the scroll issus

--- src/components/sherd/Layout.js
+++ src/components/sherd/Layout.js
@@ -5,27 +5,15 @@ import Header from './Header'
 
 const Layout = () => {
     return (
-        <div className="flex flex-row bg-neutral-100 
-        h-screen w-screen overflow-hidden">
-                <Sidber />
-
-
-                <div className="flex flex-col flex-1">
-				<Header />
-				<div className="flex-1 p-4 min-h-0 overflow-auto">
-					<Outlet />
-				</div>
-			</div>
-
-
-
-
-
-
-
-
+        <div className="bg-neutral-100 overflow-hidden flex flex-row">
+        <Sidber />
+        <div className="flex flex-col flex-1">
+            <Header />
+            <div className="flex-1 p-4 min-h-0 overflow-auto">
+                <Outlet />
+            </div>
         </div>
-
+    </div>
 
 
     )
